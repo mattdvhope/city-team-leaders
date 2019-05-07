8ddbb10a16ceb21507d5b821dbfcba1936cdeb83
mattdvhope <mattagape@me.com>
2019-05-07 20:41:42 +0700

    Added more page choices

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,15 +8,35 @@ const DashStyler = styled.div`
   font-size: 200%;
 `
 
+const RowStyler = styled.div`
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 5px solid #ccc;
+  margin: 1em 0;
+  padding-bottom: 2%;
+`
+
 export default () => {
   return (
     <DashStyler className="container">
-      <hr/>
-      <div className="row">
+      <RowStyler className="row">
         <div className="col-md-12">
           <Link to="/app/view-class-times">View class times & the students in them</Link>
         </div>
-      </div>
+      </RowStyler>
+      <RowStyler className="row">
+        <div className="col-md-12">
+          <Link to="/app/add-leader">Add Leader</Link>
+        </div>
+      </RowStyler>
+      <RowStyler className="row">
+        <div className="col-md-12">
+          <Link to="/app/list-of-leaders">See List of Leaders</Link>
+        </div>
+      </RowStyler>
+      <RowStyler className="row">
+      </RowStyler>
     </DashStyler>
   )
 }
